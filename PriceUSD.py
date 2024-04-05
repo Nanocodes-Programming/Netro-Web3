d2c35b13be0b44e79550faaaebc8fbce745f081d
Anthonyslav1 <anthony.nweke2002@gmail.com>
2024-04-05 14:48:02 +0100

    Conversion to endpoints

--- PriceUSD.py
+++ PriceUSD.py
@@ -1,70 +1,71 @@
-from requests import Request, Session
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+from flask import Flask, jsonify
+import requests
 import json
 
+app = Flask(__name__)
 
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-headers = {
-'Accepts': 'application/json',
-'X-CMC_PRO_API_KEY': 'e8e906cf-65bc-477f-bb59-71e23e7ee9d0',
-}
+@app.route('/Price-USD', methods=['GET'])
+def get_crypto_prices():
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': 'e8e906cf-65bc-477f-bb59-71e23e7ee9d0',
+    }
 
-session = Session()
-session.headers.update(headers)
+    session = requests.Session()
+    session.headers.update(headers)
 
-try:
-    response = session.get(url, params={'start' : '1', 'limit' : '1', 'convert':'USD'})
-    data = json.loads(response.text)
-    # print(data)
-    for entry in data['data']:
-        Name_BTC = entry['name']
-        symbol_BTC = entry["symbol"]
-        price_BTC = entry['quote']['USD']["price"]
-        percentage_BTC = entry['quote']['USD']["percent_change_24h"]
+    try:
+        response = session.get(url, params={'start': '1', 'limit': '1', 'convert': 'USD'})
+        data = json.loads(response.text)
+        for entry in data['data']:
+            Name_BTC = entry['name']
+            symbol_BTC = entry["symbol"]
+            price_BTC = entry['quote']['USD']["price"]
+            percentage_BTC = entry['quote']['USD']["percent_change_24h"]
 
+        response = session.get(url, params={'start': '2', 'limit': '1', 'convert': 'USD'})
+        data = json.loads(response.text)
+        for entry in data['data']:
+            Name_ETH = entry['name']
+            symbol_ETH = entry["symbol"]
+            price_ETH = entry['quote']['USD']["price"]
+            percentage_ETH = entry['quote']['USD']["percent_change_24h"]
 
-    print("{} {}: price USD is {:.2f} \nChange: {} ".format(Name_BTC, symbol_BTC, price_BTC, percentage_BTC))
-    response = session.get(url, params={'start' : '2', 'limit' : '1','convert':'USD'})
-    data = json.loads(response.text)
-    # print(data)
-    for entry in data['data']:
-        Name_ETH = entry['name']
-        symbol_ETH = entry["symbol"]
-        price_ETH = entry['quote']['USD']["price"]
-        percentage_ETH = entry['quote']['USD']["percent_change_24h"]
+        response = session.get(url, params={'start': '3', 'limit': '1', 'convert': 'USD'})
+        data = json.loads(response.text)
+        for entry in data['data']:
+            Name_USDT = entry['name']
+            symbol_USDT = entry["symbol"]
+            price_USDT = entry['quote']['USD']["price"]
+            percentage_USDT = entry['quote']['USD']["percent_change_24h"]
 
-    print("{} {}: price USD is {:.2f} \nChange: {} ".format(Name_ETH, symbol_ETH, price_ETH, percentage_ETH))
-    response = session.get(url, params={'start' : '3', 'limit' : '1','convert':'USD'})
-    data = json.loads(response.text)
-    # print(data)
-    for entry in data['data']:
-        Name_USDT = entry['name']
-        symbol_USDT = entry["symbol"]
-        price_USDT = entry['quote']['USD']["price"]
-        percentage_USDT = entry['quote']['USD']["percent_change_24h"]
+        response = session.get(url, params={'start': '15', 'limit': '1', 'convert': 'USD'})
+        data = json.loads(response.text)
+        for entry in data['data']:
+            Name_TRX = entry['name']
+            symbol_TRX = entry["symbol"]
+            price_TRX = entry['quote']['USD']["price"]
+            percentage_TRX = entry['quote']['USD']["percent_change_24h"]
 
-    print("{} {}: price USD is {:.2f} \nChange: {} ".format(Name_USDT, symbol_USDT, price_USDT, percentage_USDT))
-    response = session.get(url, params={'start' : '16', 'limit' : '1','convert':'USD'})
-    data = json.loads(response.text)
-    # print(data)
-    for entry in data['data']:
-        Name_TRX = entry['name']
-        symbol_TRX = entry["symbol"]
-        price_TRX = entry['quote']['USD']["price"]
-        percentage_TRX = entry['quote']['USD']["percent_change_24h"]
+        response = session.get(url, params={'start': '5', 'limit': '1', 'convert': 'USD'})
+        data = json.loads(response.text)
+        for entry in data['data']:
+            Name_SOL = entry['name']
+            symbol_SOL = entry["symbol"]
+            price_SOL = entry['quote']['USD']["price"]
+            percentage_SOL = entry['quote']['USD']["percent_change_24h"]
 
-    print("{} {}: price USD is {:.2f} \nChange: {} ".format(Name_TRX, symbol_TRX, price_TRX, percentage_TRX))
-    response = session.get(url, params={'start' : '4', 'limit' : '1','convert':'USD'})
-    data = json.loads(response.text)
-    # print(data)
-    for entry in data['data']:
-        Name_SOL = entry['name']
-        symbol_SOL = entry["symbol"]
-        price_SOL = entry['quote']['USD']["price"]
-        percentage_SOL = entry['quote']['USD']["percent_change_24h"]
+        return jsonify({
+            'BTC': {'name': Name_BTC, 'symbol': symbol_BTC, 'price': price_BTC, 'change': percentage_BTC},
+            'ETH': {'name': Name_ETH, 'symbol': symbol_ETH, 'price': price_ETH, 'change': percentage_ETH},
+            'USDT': {'name': Name_USDT, 'symbol': symbol_USDT, 'price': price_USDT, 'change': percentage_USDT},
+            'TRX': {'name': Name_TRX, 'symbol': symbol_TRX, 'price': price_TRX, 'change': percentage_TRX},
+            'SOL': {'name': Name_SOL, 'symbol': symbol_SOL, 'price': price_SOL, 'change': percentage_SOL},
+        })
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, requests.exceptions.TooManyRedirects) as e:
+        return jsonify({'error': str(e)}), 500
 
-    print("{} {}: price USD is {:.2f} \nChange: {} ".format(Name_SOL, symbol_SOL, price_SOL, percentage_SOL))
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-    print(e)
-
+if __name__ == '__main__':
+    app.run(debug=True)
--- priceNGN.py
+++ priceNGN.py
@@ -1,69 +1,71 @@
-from requests import Request, Session
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+from flask import Flask, jsonify
+import requests
 import json
 
-
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-headers = {
-'Accepts': 'application/json',
-'X-CMC_PRO_API_KEY': 'e8e906cf-65bc-477f-bb59-71e23e7ee9d0',
-}
-
-session = Session()
-session.headers.update(headers)
-
-try:
-    response = session.get(url, params={'start' : '1', 'limit' : '1', 'convert':'NGN'})
-    data = json.loads(response.text)
-    # print(data)
-    for entry in data['data']:
-        Name_BTC = entry['name']
-        symbol_BTC = entry["symbol"]
-        price_BTC = entry['quote']['NGN']["price"]
-        percentage_BTC = entry['quote']['NGN']["percent_change_24h"]
+app = Flask(__name__)
 
 
-    print("{} {}: price NGN is {:.2f} \nChange: {} ".format(Name_BTC, symbol_BTC, price_BTC, percentage_BTC))
-    response = session.get(url, params={'start' : '2', 'limit' : '1','convert':'NGN'})
-    data = json.loads(response.text)
-    # print(data)
-    for entry in data['data']:
-        Name_ETH = entry['name']
-        symbol_ETH = entry["symbol"]
-        price_ETH = entry['quote']['NGN']["price"]
-        percentage_ETH = entry['quote']['NGN']["percent_change_24h"]
+@app.route('/Price-NGN', methods=['GET'])
+def get_crypto_prices():
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': 'e8e906cf-65bc-477f-bb59-71e23e7ee9d0',
+    }
 
-    print("{} {}: price NGN is {:.2f} \nChange: {} ".format(Name_ETH, symbol_ETH, price_ETH, percentage_ETH))
-    response = session.get(url, params={'start' : '3', 'limit' : '1','convert':'NGN'})
-    data = json.loads(response.text)
-    # print(data)
-    for entry in data['data']:
-        Name_USDT = entry['name']
-        symbol_USDT = entry["symbol"]
-        price_USDT = entry['quote']['NGN']["price"]
-        percentage_USDT = entry['quote']['NGN']["percent_change_24h"]
+    session = requests.Session()
+    session.headers.update(headers)
 
-    print("{} {}: price NGN is {:.2f} \nChange: {} ".format(Name_USDT, symbol_USDT, price_USDT, percentage_USDT))
-    response = session.get(url, params={'start' : '16', 'limit' : '1','convert':'NGN'})
-    data = json.loads(response.text)
-    # print(data)
-    for entry in data['data']:
-        Name_TRX = entry['name']
-        symbol_TRX = entry["symbol"]
-        price_TRX = entry['quote']['NGN']["price"]
-        percentage_TRX = entry['quote']['NGN']["percent_change_24h"]
+    try:
+        response = session.get(url, params={'start': '1', 'limit': '1', 'convert': 'NGN'})
+        data = json.loads(response.text)
+        for entry in data['data']:
+            Name_BTC = entry['name']
+            symbol_BTC = entry["symbol"]
+            price_BTC = entry['quote']['NGN']["price"]
+            percentage_BTC = entry['quote']['NGN']["percent_change_24h"]
 
-    print("{} {}: price NGN is {:.2f} \nChange: {} ".format(Name_TRX, symbol_TRX, price_TRX, percentage_TRX))
-    response = session.get(url, params={'start' : '4', 'limit' : '1','convert':'NGN'})
-    data = json.loads(response.text)
-    # print(data)
-    for entry in data['data']:
-        Name_SOL = entry['name']
-        symbol_SOL = entry["symbol"]
-        price_SOL = entry['quote']['NGN']["price"]
-        percentage_SOL = entry['quote']['NGN']["percent_change_24h"]
+        response = session.get(url, params={'start': '2', 'limit': '1', 'convert': 'NGN'})
+        data = json.loads(response.text)
+        for entry in data['data']:
+            Name_ETH = entry['name']
+            symbol_ETH = entry["symbol"]
+            price_ETH = entry['quote']['NGN']["price"]
+            percentage_ETH = entry['quote']['NGN']["percent_change_24h"]
 
-    print("{} {}: price NGN is {:.2f} \nChange: {} ".format(Name_SOL, symbol_SOL, price_SOL, percentage_SOL))
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-    print(e)
+        response = session.get(url, params={'start': '3', 'limit': '1', 'convert': 'NGN'})
+        data = json.loads(response.text)
+        for entry in data['data']:
+            Name_USDT = entry['name']
+            symbol_USDT = entry["symbol"]
+            price_USDT = entry['quote']['NGN']["price"]
+            percentage_USDT = entry['quote']['NGN']["percent_change_24h"]
 
+        response = session.get(url, params={'start': '15', 'limit': '1', 'convert': 'NGN'})
+        data = json.loads(response.text)
+        for entry in data['data']:
+            Name_TRX = entry['name']
+            symbol_TRX = entry["symbol"]
+            price_TRX = entry['quote']['NGN']["price"]
+            percentage_TRX = entry['quote']['NGN']["percent_change_24h"]
+
+        response = session.get(url, params={'start': '5', 'limit': '1', 'convert': 'NGN'})
+        data = json.loads(response.text)
+        for entry in data['data']:
+            Name_SOL = entry['name']
+            symbol_SOL = entry["symbol"]
+            price_SOL = entry['quote']['NGN']["price"]
+            percentage_SOL = entry['quote']['NGN']["percent_change_24h"]
+
+        return jsonify({
+            'BTC': {'name': Name_BTC, 'symbol': symbol_BTC, 'price': price_BTC, 'change': percentage_BTC},
+            'ETH': {'name': Name_ETH, 'symbol': symbol_ETH, 'price': price_ETH, 'change': percentage_ETH},
+            'USDT': {'name': Name_USDT, 'symbol': symbol_USDT, 'price': price_USDT, 'change': percentage_USDT},
+            'TRX': {'name': Name_TRX, 'symbol': symbol_TRX, 'price': price_TRX, 'change': percentage_TRX},
+            'SOL': {'name': Name_SOL, 'symbol': symbol_SOL, 'price': price_SOL, 'change': percentage_SOL},
+        })
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout, requests.exceptions.TooManyRedirects) as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
